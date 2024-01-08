e52935f7e06a5a0385e42863de74dbfb3222b662
Kotesh <kotesh200239@gmail.com>
2024-01-08 10:09:15 +0530

    LoginSign Page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import LoginSignUp from "./components/LoginSignUp/LoginSignUp";
+
+const App = () =>{
+  return(
+    <div>
+      <LoginSignUp />
+    </div>
+  )
+}
+export default App;
